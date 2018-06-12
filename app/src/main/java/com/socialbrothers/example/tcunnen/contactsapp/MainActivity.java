1db11717cb40cd6c60047fe109169f11c539f835
Tobias Cunnen <t.cunnen@gmail.com>
2018-06-12 23:22:07 +0200

    added icons and fragements

--- app/src/main/java/com/socialbrothers/example/tcunnen/contactsapp/MainActivity.java
+++ app/src/main/java/com/socialbrothers/example/tcunnen/contactsapp/MainActivity.java
@@ -1,18 +1,19 @@
 package com.socialbrothers.example.tcunnen.contactsapp;
 
-import android.content.Intent;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     //the recyclerview
     RecyclerView recyclerView;
 
+    private SectionsStatePagerAdapter sectionsStatePagerAdapter;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,12 +36,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        sectionsStatePagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+
+        viewPager = findViewById(R.id.containter);
+
+        setupViewPager(viewPager);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                viewPager.setCurrentItem(2);
             }
         });
 
@@ -66,16 +75,22 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setupViewPager(ViewPager viewPager){
+        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AddContactFragment(), "AddContactFragment");
+        viewPager.setAdapter(adapter);
+    }
+
     private void exampleContacts(){
-        contacts.add(new Contact("Kermit the Frog","Kermit@gmail.com",19551990,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Miss Piggy","MissPiggy@gmail.com",19762002,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Fozzie Bear","FozzieBear@gmail.com",19551990,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Gonzo","Gonzo@gmail.com",19551990,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Rowlf the Dog","RowlftheDog@gmail.com",19621990,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Scooter","Scooter@gmail.com",19761991,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Pepe the King Prawn","PepetheKingPrawn@gmail.com",19761991,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Rizzo the Rat","RizzotheRat@gmail.com",19802016,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Animal","Animal@gmail.com",19752000,R.mipmap.ic_launcher_round));
-        contacts.add(new Contact("Walter","Walter@gmail.com",19761991,R.mipmap.ic_launcher_round));
+        contacts.add(new Contact("Kermit the Frog","Kermit@gmail.com",19551990,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Miss Piggy","MissPiggy@gmail.com",19762002,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Fozzie Bear","FozzieBear@gmail.com",19551990,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Gonzo","Gonzo@gmail.com",19551990,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Rowlf the Dog","RowlftheDog@gmail.com",19621990,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Scooter","Scooter@gmail.com",19761991,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Pepe the King Prawn","PepetheKingPrawn@gmail.com",19761991,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Rizzo the Rat","RizzotheRat@gmail.com",19802016,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Animal","Animal@gmail.com",19752000,R.drawable.ic_person_blue_24dp));
+        contacts.add(new Contact("Walter","Walter@gmail.com",19761991,R.drawable.ic_person_blue_24dp));
     }
 }
